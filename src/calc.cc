87c7671ffcacde6ef6be0c7f7251d72a3287cadd
Bradley Jones <brdjns@gmx.us>
2023-04-21 16:17:57 +1200

    Move constants to their own namespace

--- src/calc.cc
+++ src/calc.cc
@@ -7,6 +7,7 @@
 #include "parse.h"
 #include "symbol_table.h"
 #include "token.h"
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <limits>
@@ -21,15 +22,15 @@ try {
     // cannot be assigned to. These constants are based on the non-standard
     // 'M_*' macro constants available under <cmath> and <math.h> in many C and
     // C++ implementations.
-    names.declare("E", 2.71828182845904523536, true);       // e
-    names.declare("LOG2E", 1.44269504088896340736, true);   // log2(e)
-    names.declare("LOG10E", 0.434294481903251827651, true); // log10(e)
-    names.declare("LN2", 0.693147180559945309417, true);    // ln(2)
-    names.declare("LN10", 2.30258509299404568402, true);    // ln(10)
-    names.declare("PI", 3.14159265358979323846, true);      // pi
-    names.declare("PI_2", 1.57079632679489661923, true);    // pi/2
-    names.declare("PI_4", 0.785398163397448309616, true);   // pi/4
-    names.declare("SQRT2", 1.41421356237309504880, true);   // sqrt(2)
+    names.declare("E", Constant::e, true);
+    names.declare("LOG2E", Constant::log2e, true);
+    names.declare("LOG10E", Constant::log10e, true);
+    names.declare("LN2", Constant::ln2, true);
+    names.declare("LN10", Constant::ln10, true);
+    names.declare("PI", Constant::pi, true);
+    names.declare("PI_2", Constant::pi_2, true);
+    names.declare("PI_4", Constant::pi_4, true);
+    names.declare("SQRT2", Constant::sqrt2, true);
 
     Token_stream ts;
     compute(ts);
